2446250690e81e979c05324aeb5f85ac1c9492dd
Ilya <pletnyov.ilya@bk.ru>
2025-12-09 15:35:45 +0500

    refactor: replace @Data with @Getter and @Setter in event classes for improved clarity

--- src/main/java/com/socialnetwork/auth/dto/kafka/AccountChangedEvent.java
+++ src/main/java/com/socialnetwork/auth/dto/kafka/AccountChangedEvent.java
@@ -1,14 +1,12 @@
 package com.socialnetwork.auth.dto.kafka;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
